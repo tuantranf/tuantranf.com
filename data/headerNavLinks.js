dd1955362dbc2bf84cefcbe74ff75770108e9695
Mike Tran <tuantranf@gmail.com>
2022-12-14 01:08:34 +0700

    update blogs

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -4,6 +4,7 @@ const headerNavLinks = [
   // { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
   { href: 'https://archive.tuantranf.com/', title: 'Archive' },
+  { href: 'https://ko-fi.com/tuantranf', title: ' Buy me coffee' },
 ]
 
 export default headerNavLinks
