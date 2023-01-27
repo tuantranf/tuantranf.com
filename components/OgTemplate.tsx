c7dbbdb78eaa489b30e3c736bcb6aa6bd93c6ba5
Mike Tran <tuantranf@gmail.com>
2023-01-27 14:29:24 +0700

    add og image to blog page

--- components/OgTemplate.tsx
+++ components/OgTemplate.tsx
@@ -1,9 +1,13 @@
+import siteMetadata from '@/data/siteMetadata'
+
 export default function OgTemplate({ date, title }) {
   return (
-    <div className="flex h-[630px] w-[1200px] items-center justify-center border p-16">
+    <div className="ml-3 flex flex h-[630px] w-[1200px] items-center justify-center rounded-md border p-16 shadow">
       <div className="flex flex-col items-center justify-center text-center">
         <div className="text-2xl">
-          <div className="">{date}</div>
+          <div>
+            [{siteMetadata.headerTitle}] - {date}
+          </div>
         </div>
         <div className="mt-4 mb-8 text-center text-7xl font-extrabold leading-[80px]">{title}</div>
       </div>
